3b027c45aca92009f133b1d8e0d40e5b09e30dae
Alejandro Garcia Arvizu <alexgar0204@gmail.com>
2024-06-05 19:55:07 -0600

    player movement crear

--- CTD/Assets/Scripts/PlayerMovement.cs
+++ CTD/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float PlayerAcceleration, PlayerMaxSpeed, PlayerRunSpeed;
+    Rigidbody PlayerRigidBody;
+
+    private float horizontalInput, verticalInput, PlayerRunSpeedFake = 1;
+    private bool canMove = true;
+    Vector3 playerVelocity;
+
+    private void Start()
     {
-        
+        PlayerRigidBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Dash();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        playerVelocity = PlayerRigidBody.velocity;
+        playerVelocity.x = Mathf.Clamp(playerVelocity.x, -PlayerMaxSpeed * PlayerRunSpeedFake, PlayerMaxSpeed * PlayerRunSpeedFake);
+        playerVelocity.z = Mathf.Clamp(playerVelocity.z, -PlayerMaxSpeed * PlayerRunSpeedFake, PlayerMaxSpeed * PlayerRunSpeedFake);
+        playerVelocity.y = 0;
+        if (horizontalInput == 0f || verticalInput == 0f)
+        {
+            PlayerRunSpeedFake = 1;
+        }
+        PlayerRigidBody.velocity = playerVelocity;
+
+        if (canMove)
+        {
+            PlayerRigidBody.AddForce(Vector3.right * horizontalInput * PlayerAcceleration * PlayerRunSpeedFake, ForceMode.Force);
+            PlayerRigidBody.AddForce(0, 0, verticalInput * PlayerAcceleration * PlayerRunSpeedFake, ForceMode.Force);
+        }
+    }
+
+    private void Dash()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            PlayerRunSpeedFake = PlayerRunSpeed;
+        }
     }
 }
