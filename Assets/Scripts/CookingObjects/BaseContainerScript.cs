5bc8e845afb0f78b21702fd0cbcb7590ed1903c3
DavidElPapu <david.jung@uniat.edu.mx>
2024-07-07 22:26:35 -0600

    scripts para diferentes ingredientes aun no terminado

--- Assets/Scripts/CookingObjects/BaseContainerScript.cs
+++ Assets/Scripts/CookingObjects/BaseContainerScript.cs
@@ -18,119 +18,119 @@ public class BaseContainerScript : MonoBehaviour
     private List<GameObject> containedGameObjects = new();
     private int towerInContainerID = -1;
 
-    public bool CanPlaceGameObjectsInContainer(GameObject[] gameObjects)
-    {
-        if (towerInContainerID != -1)
-            return false;
-        foreach (GameObject gameObject in gameObjects)
-        {
-            int gameObjectID = gameObject.GetComponent<BaseIngredientScript>().GetIngredientID();
-            BaseIngredientScript.IngredientState gameObjectState = gameObject.GetComponent<BaseIngredientScript>().GetCurrentState();
-            if ((gameObjectID >= 200 && gameObjectID < 300) && !canContainIngredients)
-                return false;
-            if ((gameObjectID >= 300 && gameObjectID < 400) && !canContainSpices)
-                return false;
-            if (gameObjectState == BaseIngredientScript.IngredientState.defaultState && !canContainDefault)
-                return false;
-            if (gameObjectState == BaseIngredientScript.IngredientState.cutState && !canContainCut)
-                return false;
-            if (gameObjectState == BaseIngredientScript.IngredientState.cookedState && !canContainCooked)
-                return false;
-            foreach (GameObject containedGameObject in containedGameObjects)
-            {
-                if (gameObjectID == containedGameObject.GetComponent<BaseIngredientScript>().GetIngredientID())
-                    return false;
-            }
-        }
-        //Si todos los objetos pueden entrar, los agrega todos, si tan solo 1 objeto no puede entrar, entonces no deja.
-        foreach (GameObject gameObject in gameObjects)
-        {
-            containedGameObjects.Add(gameObject);
-            ShowGameObjectInContainer(gameObject.GetComponent<BaseIngredientScript>().GetIngredientID(), gameObject.GetComponent<BaseIngredientScript>().GetCurrentState());
-        }
-        return true;
-    }
+    //public bool CanPlaceGameObjectsInContainer(GameObject[] gameObjects)
+    //{
+    //    if (towerInContainerID != -1)
+    //        return false;
+    //    foreach (GameObject gameObject in gameObjects)
+    //    {
+    //        int gameObjectID = gameObject.GetComponent<BaseIngredientScript>().GetIngredientID();
+    //        BaseIngredientScript.IngredientState gameObjectState = gameObject.GetComponent<BaseIngredientScript>().GetCurrentState();
+    //        if ((gameObjectID >= 200 && gameObjectID < 300) && !canContainIngredients)
+    //            return false;
+    //        if ((gameObjectID >= 300 && gameObjectID < 400) && !canContainSpices)
+    //            return false;
+    //        if (gameObjectState == BaseIngredientScript.IngredientState.defaultState && !canContainDefault)
+    //            return false;
+    //        if (gameObjectState == BaseIngredientScript.IngredientState.cutState && !canContainCut)
+    //            return false;
+    //        if (gameObjectState == BaseIngredientScript.IngredientState.cookedState && !canContainCooked)
+    //            return false;
+    //        foreach (GameObject containedGameObject in containedGameObjects)
+    //        {
+    //            if (gameObjectID == containedGameObject.GetComponent<BaseIngredientScript>().GetIngredientID())
+    //                return false;
+    //        }
+    //    }
+    //    //Si todos los objetos pueden entrar, los agrega todos, si tan solo 1 objeto no puede entrar, entonces no deja.
+    //    foreach (GameObject gameObject in gameObjects)
+    //    {
+    //        containedGameObjects.Add(gameObject);
+    //        ShowGameObjectInContainer(gameObject.GetComponent<BaseIngredientScript>().GetIngredientID(), gameObject.GetComponent<BaseIngredientScript>().GetCurrentState());
+    //    }
+    //    return true;
+    //}
 
-    private void ShowGameObjectInContainer(int id, BaseIngredientScript.IngredientState ingredientState)
-    {
-        if (id >= 200 && id < 300)
-        {
-            switch (ingredientState)
-            {
-                case BaseIngredientScript.IngredientState.defaultState:
-                    defaultIngredientsInContainer[id - 200].SetActive(true);
-                    break;
-                case BaseIngredientScript.IngredientState.cutState:
-                    cutIngredientsInContainer[id - 200].SetActive(true);
-                    break;
-                case BaseIngredientScript.IngredientState.cookedState:
-                    cookedIngredientsInContainer[id - 200].SetActive(true);
-                    break;
-            }
-        }
-        else if (id >= 300 && id < 400)
-        {
-            switch (ingredientState)
-            {
-                case BaseIngredientScript.IngredientState.defaultState:
-                    defaultSpicesInContainer[id - 300].SetActive(true);
-                    break;
-                case BaseIngredientScript.IngredientState.cutState:
-                    cutSpicesInContainer[id - 300].SetActive(true);
-                    break;
-                case BaseIngredientScript.IngredientState.cookedState:
-                    cookedSpicesInContainer[id - 300].SetActive(true);
-                    break;
-            }
-        }
-    }
+    //private void ShowGameObjectInContainer(int id, BaseIngredientScript.IngredientState ingredientState)
+    //{
+    //    if (id >= 200 && id < 300)
+    //    {
+    //        switch (ingredientState)
+    //        {
+    //            case BaseIngredientScript.IngredientState.defaultState:
+    //                defaultIngredientsInContainer[id - 200].SetActive(true);
+    //                break;
+    //            case BaseIngredientScript.IngredientState.cutState:
+    //                cutIngredientsInContainer[id - 200].SetActive(true);
+    //                break;
+    //            case BaseIngredientScript.IngredientState.cookedState:
+    //                cookedIngredientsInContainer[id - 200].SetActive(true);
+    //                break;
+    //        }
+    //    }
+    //    else if (id >= 300 && id < 400)
+    //    {
+    //        switch (ingredientState)
+    //        {
+    //            case BaseIngredientScript.IngredientState.defaultState:
+    //                defaultSpicesInContainer[id - 300].SetActive(true);
+    //                break;
+    //            case BaseIngredientScript.IngredientState.cutState:
+    //                cutSpicesInContainer[id - 300].SetActive(true);
+    //                break;
+    //            case BaseIngredientScript.IngredientState.cookedState:
+    //                cookedSpicesInContainer[id - 300].SetActive(true);
+    //                break;
+    //        }
+    //    }
+    //}
 
-    public void EmptyContainer(bool deleteGameObjects)
-    {
-        if (deleteGameObjects)
-        {
-            foreach (GameObject gameObject in containedGameObjects)
-            {
-                HideGameObjectInContainer(gameObject.GetComponent<BaseIngredientScript>().GetIngredientID(), gameObject.GetComponent<BaseIngredientScript>().GetCurrentState());
-                Destroy(gameObject);
-            }
-        }
-        containedGameObjects.Clear();
-    }
+    //public void EmptyContainer(bool deleteGameObjects)
+    //{
+    //    if (deleteGameObjects)
+    //    {
+    //        foreach (GameObject gameObject in containedGameObjects)
+    //        {
+    //            HideGameObjectInContainer(gameObject.GetComponent<BaseIngredientScript>().GetIngredientID(), gameObject.GetComponent<BaseIngredientScript>().GetCurrentState());
+    //            Destroy(gameObject);
+    //        }
+    //    }
+    //    containedGameObjects.Clear();
+    //}
 
-    private void HideGameObjectInContainer(int id, BaseIngredientScript.IngredientState ingredientState)
-    {
-        if (id >= 200 && id < 300)
-        {
-            switch (ingredientState)
-            {
-                case BaseIngredientScript.IngredientState.defaultState:
-                    defaultIngredientsInContainer[id - 200].SetActive(false);
-                    break;
-                case BaseIngredientScript.IngredientState.cutState:
-                    cutIngredientsInContainer[id - 200].SetActive(false);
-                    break;
-                case BaseIngredientScript.IngredientState.cookedState:
-                    cookedIngredientsInContainer[id - 200].SetActive(false);
-                    break;
-            }
-        }
-        else if (id >= 300 && id < 400)
-        {
-            switch (ingredientState)
-            {
-                case BaseIngredientScript.IngredientState.defaultState:
-                    defaultSpicesInContainer[id - 300].SetActive(false);
-                    break;
-                case BaseIngredientScript.IngredientState.cutState:
-                    cutSpicesInContainer[id - 300].SetActive(false);
-                    break;
-                case BaseIngredientScript.IngredientState.cookedState:
-                    cookedSpicesInContainer[id - 300].SetActive(false);
-                    break;
-            }
-        }
-    }
+    //private void HideGameObjectInContainer(int id, BaseIngredientScript.IngredientState ingredientState)
+    //{
+    //    if (id >= 200 && id < 300)
+    //    {
+    //        switch (ingredientState)
+    //        {
+    //            case BaseIngredientScript.IngredientState.defaultState:
+    //                defaultIngredientsInContainer[id - 200].SetActive(false);
+    //                break;
+    //            case BaseIngredientScript.IngredientState.cutState:
+    //                cutIngredientsInContainer[id - 200].SetActive(false);
+    //                break;
+    //            case BaseIngredientScript.IngredientState.cookedState:
+    //                cookedIngredientsInContainer[id - 200].SetActive(false);
+    //                break;
+    //        }
+    //    }
+    //    else if (id >= 300 && id < 400)
+    //    {
+    //        switch (ingredientState)
+    //        {
+    //            case BaseIngredientScript.IngredientState.defaultState:
+    //                defaultSpicesInContainer[id - 300].SetActive(false);
+    //                break;
+    //            case BaseIngredientScript.IngredientState.cutState:
+    //                cutSpicesInContainer[id - 300].SetActive(false);
+    //                break;
+    //            case BaseIngredientScript.IngredientState.cookedState:
+    //                cookedSpicesInContainer[id - 300].SetActive(false);
+    //                break;
+    //        }
+    //    }
+    //}
 
     public void GetTowerInContainer(int towerID)
     {
---
+++ Assets/Scripts/CookingObjects/IngredientStates/OnlyPotIngredientState.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnlyPotIngredientState : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] ingredientModels00;
+    // 0 - default
+    // 1 - cocido
+    private GameObject lastModel;
+
+    [SerializeField]
+    private int ingredientState3 = 0;
+    //Solo 1 de estas a la vez
+    // 0 - default
+    // 1 - cocido/cocinado (en olla)
+
+    public void ChangeState3(int newState)
+    {
+        ingredientState3 = newState;
+        lastModel.SetActive(false);
+        ingredientModels00[ingredientState3].SetActive(true);
+        lastModel = ingredientModels00[ingredientState3];
+    }
+
+    public int ShowState3()
+    {
+        return ingredientState3;
+    }
+}
