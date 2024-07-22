771578053f3726299143a7c1878a88fb6f10d269
unknown <alexgar0204@gmail.com>
2024-07-22 13:36:21 -0600

    El prototipo

--- Assets/Scripts/KitchenLife.cs
+++ Assets/Scripts/KitchenLife.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class KitchenLife : MonoBehaviour
 {
     public float KitchenHealth;
+
+    private void FixedUpdate()
+    {
+        if (KitchenHealth <= 0) 
+        {
+            Time.timeScale = 0.0f;
+        }
+    }
 
     public void TakeDamage(float damage)
     {
